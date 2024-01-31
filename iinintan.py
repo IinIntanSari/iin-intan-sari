8e12801e78e4cf61dd876433ca32a2461ce9d475
IinIntanSari <158256993+IinIntanSari@users.noreply.github.com>
2024-01-31 15:09:15 +0700

    Update iinintan.py

--- iinintan.py
+++ iinintan.py
@@ -24,7 +24,23 @@ st.pyplot(fig)
 #subheader
 st.subheader("Plot Sin & cos")
 
-with co12:
+with co11:
+  st.caption('Plot Sin')
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*pi
+  y = np.cos(x) # Calculating sin(x) values
+
+fig, ax = plt.sublots(figsize=(16, 8))
+ax.plot(x, y, label='sin(x)', color='b') # plotting sin(x) curve
+ax.set_ylabel("sin x")
+ax.set_ylabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis'x', labelsize=15)
+
+st.pyplot(fig)
+
+
+with col2:
   x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*
   y = np.cos(x) # Calculating sin(x) values
 
@@ -38,3 +54,4 @@ ax.tick_params(axis'x', labelsize=15)
 
 st.pyplot(fig)
 
+st.caption('copyright = IIN INTAN INSAEI 2024')
