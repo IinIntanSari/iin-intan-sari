d4e860093f711d621b028b7bda5b11575fddf576
IinIntanSari <158256993+IinIntanSari@users.noreply.github.com>
2024-01-31 15:03:27 +0700

    Update iinintan.py

--- iinintan.py
+++ iinintan.py
@@ -10,7 +10,7 @@ x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*
 y = np.sin(x) # Calculating sin(x) values
 z = np.cos(x) # Calculating sin(x) values
 
-fig, ax = ply.sublots(figsize=(16, 8))
+fig, ax = plt.sublots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b') # plotting sin(x) curve
 ax.plot(x, z, label='cos(x)', color='g') # plotting sin(x) curve
 ax.set_ylabel("")
@@ -19,4 +19,22 @@ ax.tick_params(axis='y', labelsize=20)
 ax.tick_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis'x', labelsize=15)
 
-st.phplot(fig)
+st.pyplot(fig)
+
+#subheader
+st.subheader("Plot Sin & cos")
+
+with co12:
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*
+  y = np.cos(x) # Calculating sin(x) values
+
+fig, ax = plt.sublots(figsize=(16, 8))
+ax.plot(x, y, label='cos(x)', color='g') # plotting cos(x) curve
+ax.set_ylabel("cos x")
+ax.set_ylabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis'x', labelsize=15)
+
+st.pyplot(fig)
+
