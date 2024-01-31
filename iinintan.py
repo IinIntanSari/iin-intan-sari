bfa598648414d1ca143bd6bb0f80ae2c9810f88f
IinIntanSari <158256993+IinIntanSari@users.noreply.github.com>
2024-01-31 14:23:25 +0700

    Create iinintan.py

---
+++ iinintan.py
@@ -0,0 +1,22 @@
+import numpy as np
+inport matplotlib.pyblot as plt
+import streamlit as st
+
+# Header
+st.header('IinIntan : sparkles :')
+st.subheader('Plot')
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*
+y = np.sin(x) # Calculating sin(x) values
+z = np.cos(x) # Calculating sin(x) values
+
+fig, ax = ply.sublots(figsize=(16, 8))
+ax.plot(x, y, label='sin(x)', color='b') # plotting sin(x) curve
+ax.plot(x, z, label='cos(x)', color='g') # plotting sin(x) curve
+ax.set_ylabel("")
+ax.set_ylabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axiz'x', labelsize=15)
+
+st.phplot(fig)
