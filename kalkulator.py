6a1a53ee814411d3c3facf75593a0dd95169366d
IinIntanSari <158256993+IinIntanSari@users.noreply.github.com>
2024-02-07 14:29:48 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -28,6 +28,16 @@ def calculate(num1, num2, operation):
     result = num1 - num2
   elif operation == 'x':
      result = num1 * num2
-  lif operation == '/':
+  elif operation == '/':
+    if num2 != 0: # Menghindari pembagian dengan nol
+      result = num1 / num2
+    else:
+      result = "Error: Pembagian dengan nol"
+      return result
+
+result = calculate(num1, num2, operation)
+st.write('Hasil:' result)
+
+st.caption('copyright © Iin Intan Insari 2024')
 
 
